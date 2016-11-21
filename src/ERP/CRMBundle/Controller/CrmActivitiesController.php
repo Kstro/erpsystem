d867b6cb5f77f46f4fa292964ef1c482cb8f31f2
Digitality 5 <Digitality 5@192.168.0.8>
2016-11-21 08:49:02 -0600

    Actualización de proyecto

--- src/ERP/CRMBundle/Controller/CrmActivitiesController.php
+++ src/ERP/CRMBundle/Controller/CrmActivitiesController.php
@@ -49,7 +49,7 @@ class CrmActivitiesController extends Controller
                 'recordatorios'=>$recordatorios,
                 'tiempos'=>$tiempos,
                 'prioridad'=>$prioridad,
-                'actividades'=>$tipoActividades,
+                'crmActividades'=>$tipoActividades,
                 'menuTodasActividadesA'=>true,
             ));
         
