1d48bc36b1cdd4aa4676420575eafaac17d48f36
ebos12 <elman.ortiz@gmail.com>
2016-11-25 12:10:57 -0600

    list de gestion productos

--- src/ERP/CRMBundle/Controller/CtlProductoController.php
+++ src/ERP/CRMBundle/Controller/CtlProductoController.php
@@ -2,10 +2,13 @@
 
 namespace ERP\CRMBundle\Controller;
 
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use ERP\CRMBundle\Entity\CtlProducto;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;use Symfony\Component\HttpFoundation\Request;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
 
 /**
  * Ctlproducto controller.
@@ -27,7 +30,8 @@ class CtlProductoController extends Controller
         $ctlProductos = $em->getRepository('ERPCRMBundle:CtlProducto')->findAll();
 
         return $this->render('ctlproducto/index.html.twig', array(
-            'ctlProductos' => $ctlProductos,
+            //'ctlProductos' => $ctlProductos,
+            'menuCtlProductoA' => true,
         ));
     }
 
@@ -133,4 +137,102 @@ class CtlProductoController extends Controller
             ->getForm()
         ;
     }
+    
+    /**
+     * List contact
+     *
+     * @Route("/campaigns/data/list", name="admin_contact_data")
+    */
+    public function datacontactsAction(Request $request) {
+        try {
+            $start = $request->query->get('start');
+            $draw = $request->query->get('draw');
+            $longitud = $request->query->get('length');
+            $busqueda = $request->query->get('search');
+
+            $em = $this->getDoctrine()->getEntityManager();
+            $rowsTotal = $em->getRepository('ERPCRMBundle:CtlProducto')->findBy(array('estado' => 1));
+            
+            $row['draw'] = $draw++;
+            $row['recordsTotal'] = count($rowsTotal);
+            $row['recordsFiltered'] = count($rowsTotal);
+            $row['data'] = array();
+
+            $arrayFiltro = explode(' ', $busqueda['value']);
+
+            $orderParam = $request->query->get('order');
+            $orderBy = $orderParam[0]['column'];
+            $orderDir = $orderParam[0]['dir'];
+            //var_dump($orderDir);
+            $orderByText = "";
+            switch (intval($orderBy)) {
+                case 1:
+                    $orderByText = "name";
+                    break;
+
+                case 2:
+                    $orderByText = "state";
+                    break;
+            }
+            $busqueda['value'] = str_replace(' ', '%', $busqueda['value']);
+            if ($busqueda['value'] != '') {
+                $sql = "SELECT CONCAT('<div id=\"',obj.id,'\" style=\"text-align:left\"><input style=\"z-index:5;\" class=\"chkItem\" type=\"checkbox\"></div>') as chk, CONCAT('<div style=\"text-align:left\">',obj.nombre,'</div>') as name,
+                                CASE
+                                WHEN obj.estado =1 THEN 'Active'
+                                ELSE 'Inactive'
+                                END AS state FROM ERPCRMBundle:CtlProducto obj "
+                        . "WHERE obj.estado=1 AND upper(obj.nombre) LIKE upper(:busqueda) "
+                        . "AND obj.estado=1 ORDER BY " . $orderByText . " " . $orderDir;
+                $row['data'] = $em->createQuery($sql)
+                        ->setParameters(array('busqueda' => "%" . $busqueda['value'] . "%"))
+                        ->getResult();
+                $row['recordsFiltered'] = count($row['data']);
+                $sql = "SELECT CONCAT('<div id=\"',obj.id,'\" style=\"text-align:left\"><input style=\"z-index:5;\" class=\"chkItem\" type=\"checkbox\"></div>') as chk, CONCAT('<div style=\"text-align:left\">',obj.nombre,'</div>') as name,
+                                CASE
+                                WHEN obj.estado =1 THEN 'Active'
+                                ELSE 'Inactive'
+                                END AS state FROM ERPCRMBundle:CtlProducto obj "
+                        . "WHERE obj.estado=1 AND upper(obj.nombre) LIKE upper(:busqueda) "
+                        . "AND pac.estado=1 ORDER BY " . $orderByText . " " . $orderDir;
+                $row['data'] = $em->createQuery($sql)
+                        ->setParameters(array('busqueda' => "%" . $busqueda['value'] . "%"))
+                        ->setFirstResult($start)
+                        ->setMaxResults($longitud)
+                        ->getResult();
+            } else {
+                $sql = "SELECT CONCAT('<div id=\"',obj.id,'\" style=\"text-align:left\"><input style=\"z-index:5;\" class=\"chkItem\" type=\"checkbox\"></div>') as chk, CONCAT('<div style=\"text-align:left\">',obj.nombre,'</div>') as name,
+                                CASE
+                                WHEN obj.estado =1 THEN 'Active'
+                                ELSE 'Inactive'
+                                END AS state FROM ERPCRMBundle:CtlProducto obj "
+                        . " WHERE obj.estado=1 ORDER BY " . $orderByText . " " . $orderDir;
+                $row['data'] = $em->createQuery($sql)
+                        ->setFirstResult($start)
+                        ->setMaxResults($longitud)
+                        ->getResult();
+            }
+            //var_dump($row);
+            return new Response(json_encode($row));
+        } catch (\Exception $e) {
+            //var_dump($e);
+            if (method_exists($e, 'getErrorCode')) {
+                switch (intval($e->getErrorCode())) {
+                    case 2003:
+                        $serverOffline = $this->getParameter('app.serverOffline');
+                        $row['data'][0]['name'] = $serverOffline . '. CODE: ' . $e->getErrorCode();
+                        break;
+                    default :
+                        $row['data'][0]['name'] = $e->getMessage();
+                        break;
+                }
+                $row['data'][0]['chk'] = '';
+
+                $row['recordsFiltered'] = 0;
+            } else {
+                $data['error'] = $e->getMessage();
+            }
+            return new Response(json_encode($row));
+        }
+    }
+
 }
